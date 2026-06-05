494e699556c92db9af697bb2afb2e0631cb8ea48
Tarde <https://github.com/danielriosg260-hash/apibasica344.git>
2026-06-05 15:10:08 -0500

    Primer commit

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 
 const clienteSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  telefono: { type: String }
+  nombre: {
+    type: String,
+    required: [true, 'El nombre es obligatorio']
+  },
+
+  email: {
+    type: String,
+    required: [true, 'El email es obligatorio'],
+    unique: [true, 'El email ya está registrado']
+  },
+
+  telefono: {
+    type: String,
+    required: [true, 'El teléfono es obligatorio'],
+    minlength: [9, 'El teléfono debe tener al menos 9 dígitos'],
+    maxlength: [10, 'El teléfono no puede tener más de 10 dígitos']
+  }
 });
 
 module.exports = mongoose.model('Cliente', clienteSchema);
